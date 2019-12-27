05b486a8b2e66fe5105270c28a6c6788ef9744c5
share-with-me <monish1247@gmail.com>
2019-12-27 10:16:18 -0800

    Cloudfront url change

--- static/E6156/index.js
+++ static/E6156/index.js
@@ -1,5 +1,5 @@
 function login_using_fb_google()
 {
-    window.location.href = 'https://tse-final.auth.us-east-1.amazoncognito.com/login?client_id=8l08204jb7op0c3fbiv4io1hu&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://tse-frontend-shubham.s3.amazonaws.com/static/E6156/index.html';
+    window.location.href = 'https://tse-final.auth.us-east-1.amazoncognito.com/login?client_id=8l08204jb7op0c3fbiv4io1hu&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://d2osysv0xzc20k.cloudfront.net/index.html';
     console.log("hello - will now get the token from the URL");
 }
